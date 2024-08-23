a06e71900a2bea889d81cf4c67fc6b0deb9ded40
Md. Ariful Islam <arifislam11ctg@gmail.com>
2024-08-24 04:32:48 +0600

    tested10

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -6,6 +6,8 @@ const sendToken = (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // Should be true for HTTPS
+    sameSite: "None",
   };
 
   res
