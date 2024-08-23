a9a84edb72c61e6e8021bd77e444c0c6fda96a82
Md. Ariful Islam <arifislam11ctg@gmail.com>
2024-08-24 01:30:52 +0600

    tested3

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(fileUpload());
 // CORS Configuration
 // This setup handles CORS preflight requests manually
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://sciencetent.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://sciencetent.vercel.app/");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
