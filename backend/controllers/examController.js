1bde0e3ef4b34e510b4d75802f5f1face20367aa
Md. Ariful Islam <arifislam11ctg@gmail.com>
2024-12-11 00:21:24 +0600

    marks input section is ready for test10

--- backend/controllers/examController.js
+++ backend/controllers/examController.js
@@ -74,27 +74,27 @@ exports.batchWiseMarksInput = catchAsyncError(async (req, res, next) => {
     return next(new ErrorHandler(`Invalid data for marks`, 400));
   }
    
-   exam = await Exam.findByIdAndUpdate(req.params.examId, {result: JSON.parse(req.body.allMarks)}, {
-    new: true,
-    runValidators: true,
-    useFindAndModify: false,
-  });
+  //  exam = await Exam.findByIdAndUpdate(req.params.examId, {result: JSON.parse(req.body.allMarks)}, {
+  //   new: true,
+  //   runValidators: true,
+  //   useFindAndModify: false,
+  // });
   // Prepare data for insertion into the Result array
-  // const resultEntries = allMarks.map((mark) => ({
-  //   student: mark.student,
-  //   courses: mark.courses.map((course) => ({
-  //     course: course.courseId, // Ensure you send courseId in the request
-  //     marks: course.marks,
-  //   })),
-  //   batch: mark.batch,
-  // }));
+  const resultEntries = allMarks.map((mark) => ({
+    student: mark.student,
+    courses: mark.courses.map((course) => ({
+      course: course.courseId, // Ensure you send courseId in the request
+      marks: course.marks,
+    })),
+    batch: mark.batch,
+  }));
 
   // Update the Result array
-  // await Exam.findByIdAndUpdate(
-  //   req.params.examId,
-  //   { $push: { result: { $each: resultEntries } } }, // Push multiple entries
-  //   { new: true, runValidators: true }
-  // );
+  await Exam.findByIdAndUpdate(
+    req.params.examId,
+    { $push: { result: { $each: resultEntries } } }, // Push multiple entries
+    { new: true, runValidators: true }
+  );
 
   res.status(200).json({
     success: true,
