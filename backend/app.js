69eb2a1599fd0fc007820075527eb0e12c9cda05
Md. Ariful Islam <arifislam11ctg@gmail.com>
2024-12-13 02:42:51 +0600

    testing marks input backend17

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(fileUpload());
 // This setup handles CORS preflight requests manually
 // CORS Configuration using cors middleware
 const corsOptions = {
-  origin: "*",
+  origin: ["https://sciencetent.vercel.app", "http://localhost:3000"],
   methods: "GET, POST, PUT, DELETE, OPTIONS",
   allowedHeaders: ["Content-Type", "Authorization", "cookie"],
   credentials: true,
