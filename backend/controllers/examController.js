41207893c4ce4899f01c8f84f8b283e4a32b412b
Md. Ariful Islam <arifislam11ctg@gmail.com>
2024-12-13 00:53:43 +0600

    testing marks input backend6

--- backend/controllers/examController.js
+++ backend/controllers/examController.js
@@ -64,9 +64,9 @@ const Batch = require("../models/batchModel")
 
 exports.batchWiseMarksInput = catchAsyncError(async (req, res, next) => {
   const { resultData } = req.body;
-  console.log(typeof(resultData))
-  console.log(typeof(req.body.resultData))
-  console.log(req.body.resultData)
+  // console.log(typeof(resultData))
+  // console.log(typeof(req.body.resultData))
+  // console.log(req.body.resultData)
   // Check if the exam exists
   const exam = await Exam.findById(req.params.examId);
   if (!exam) {
@@ -74,11 +74,29 @@ exports.batchWiseMarksInput = catchAsyncError(async (req, res, next) => {
   }
 
   // Validate allMarks structure
-  console.log(resultData.batchId,'==========',resultData.batchWiseResult.length)
+  // console.log(resultData.batchId,'==========',resultData.batchWiseResult.length)
   if (resultData.batchId === '' || resultData.batchWiseResult.length === 0) {
     return next(new ErrorHandler(`Invalid data for marks`, 400));
   }
 
+  exam.result.map(async (item)=>{
+    if(resultData.batchId === item.batchId){
+      res.status(200).json({
+        success: true,
+        message: "Marks already inputted for this batch",
+      });
+    }else{
+      await Exam.findByIdAndUpdate(
+        req.params.examId,
+        {
+          $push: {
+            result: req.body.resultData, // Replacing the entire result array
+          },
+        },
+        { new: true, useFindAndModify: false }
+      );
+    }
+  })
   // Prepare data for insertion into the Result array
 
   // const resultEntries = allMarks.map((mark) => ({
@@ -97,15 +115,7 @@ exports.batchWiseMarksInput = catchAsyncError(async (req, res, next) => {
   // Overwrite the Result array with the new data
 
 
-  await Exam.findByIdAndUpdate(
-    req.params.examId,
-    {
-      $push: {
-        result: req.body.resultData, // Replacing the entire result array
-      },
-    },
-    { new: true, useFindAndModify: false }
-  );
+  
 
   res.status(200).json({
     success: true,
