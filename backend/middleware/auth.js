21e73897f2b51a9f73e3b72d6e5e4f9648796e31
Md. Ariful Islam <arifislam11ctg@gmail.com>
2025-03-09 00:41:09 +0600

    token updated

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -21,6 +21,7 @@ exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
   next();
 });
 exports.isUserLoaded = catchAsyncError(async (req, res, next) => {
+  const { token } = req.cookies;
 
   if (!token) {
     // return next(new ErrorHandler("Please Login to access this resource", 401));
